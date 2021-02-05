75784f96c3c691cd4a71a8db02cc3bd2311587d8
unknown <rnjsguswo012@hufs.ac.kr>
2021-02-05 18:03:42 +0900

    Feat id condition

--- index.py
+++ index.py
@@ -3,7 +3,10 @@
 print("content-type:text/html; charset=UTF-8\n")
 import cgi
 form = cgi.FieldStorage()
-pageId = form["id"].value
+if "id" in form:
+    pageId = form["id"].value
+else:
+    pageId = 'Welcome'
 
 print('''<!doctype html>
 <html>
